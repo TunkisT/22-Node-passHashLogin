12af601d6992695f3cebfe4f4bfea0e037bff93c
Tautvydas Tunkis <ttunkis@gmail.com>
2022-03-02 16:51:24 +0200

    pass check

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 // const mysql = require('mysql2/promise');
 // const dbConfig = require('./dbConfig');
@@ -40,7 +41,10 @@ app.get('/users/:username', (req, res) => {
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
   const userObjFound = users.find((userObj) => userObj.username === username);
-  if (userObjFound && password === userObjFound.password) {
+  // if (bcrypt.compareSync(password, userObjFound.password)) {
+  //   console.log('sutampa');
+  // }
+  if (bcrypt.compareSync(password, userObjFound.password) && userObjFound) {
     res.json('Login success');
   } else {
     res.status(400).send('username or password not match');
@@ -49,12 +53,14 @@ app.post('/login', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
+  const passHash = bcrypt.hashSync(password, 10);
+  console.log('passHash ===', passHash);
   const newUser = {
     username,
-    password,
+    password: passHash,
   };
   users.push(newUser);
-  res.send('Register success');
+  res.send(passHash);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
