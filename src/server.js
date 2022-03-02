6391859a48e9b9f6210a9b5790d07713c5083b57
Tautvydas Tunkis <ttunkis@gmail.com>
2022-03-02 15:31:13 +0200

    username id

--- src/server.js
+++ src/server.js
@@ -9,13 +9,32 @@ const PORT = process.env.SERVER_PORT || 3000;
 
 const app = express();
 
+const users = [
+  {
+    id: 1,
+    username: 'Mike',
+    password: 'secret',
+  },
+  {
+    id: 2,
+    username: 'James',
+    password: 'jam',
+  },
+];
+
 // middleware
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello express');
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.get('/users/:username', (req, res) => {
+  const name = req.params.username;
+  const userObjFound = users.find((userObj) => userObj.username === name);
+  res.json(userObjFound);
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
