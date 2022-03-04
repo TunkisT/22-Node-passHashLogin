f75c1059060d52e8769eb95464b16ee7cac3a04c
Tautvydas Tunkis <ttunkis@gmail.com>
2022-03-04 14:07:39 +0200

    +

--- public/js/login.js
+++ public/js/login.js
@@ -2,18 +2,21 @@ const forma = document.forms.login;
 
 forma.addEventListener('submit', (event) => {
   event.preventDefault();
-  const info = {
+  const loginData = {
     username: event.target.elements.username.value,
     password: event.target.elements.password.value,
   };
-  console.log(info);
-  fetch('http://localhost:3000/login', {
+  loginUser(loginData);
+});
+
+async function loginUser(loginData) {
+  console.log(loginData);
+
+  const resp = await fetch('http://localhost:3000/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(info),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    });
-});
+    body: JSON.stringify(loginData),
+  });
+  const respInJs = await resp.json();
+  console.log('respInJs ===', respInJs);
+}
